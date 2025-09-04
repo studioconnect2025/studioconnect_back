480b81edc3835cb990e2fe224e8472224c1b9104
RafaelAlvarezSM <alvarezlopezsm@gmail.com>
2025-09-04 18:12:47 -0500

    imports/TypeOrmModule.forFeacture/en-los-modulos/instruments-seeder-y-category

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SeederService } from './seeder.service';
 import { SeederController } from './seeder.controller';
+import { Booking } from 'src/bookings/dto/bookings.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/categories/entities/category.entity';
+import { Instruments } from 'src/instrumentos/entities/instrumento.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Instruments, Category, Booking])],
   controllers: [SeederController],
   providers: [SeederService],
 })
