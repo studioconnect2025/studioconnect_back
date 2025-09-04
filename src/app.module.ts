7a97d2a08969619664f8c02aed562e01fea0fe90
RafaelAlvarezSM <alvarezlopezsm@gmail.com>
2025-09-03 20:36:04 -0500

    Guardando cambios antes de cambiar de rama

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,19 @@ import { InstrumentosModule } from './instrumentos/instrumentos.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { StudiosModule } from './studios/studios.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SeederModule } from './seeder/seeder.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
   imports: [
     InstrumentosModule,
     UsersModule,
     AuthModule,
-    StudiosModule
+    StudiosModule,
+    CategoriesModule,
+    SeederModule,
+    BookingsModule
   ],
   controllers: [AppController],
   providers: [AppService],
