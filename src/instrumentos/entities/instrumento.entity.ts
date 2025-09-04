4ac697f39880ff4731647d2b451c6d7fed5e04ed
RafaelAlvarezSM <alvarezlopezsm@gmail.com>
2025-09-03 19:25:48 -0500

    Terminado módulo Instrumentos y precarga de Categorías/Seeder

--- src/instrumentos/entities/instrumento.entity.ts
+++ src/instrumentos/entities/instrumento.entity.ts
@@ -1,23 +1,48 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Studio } from 'src/studios/entities/studio.entity';
+import { Category } from 'src/categories/entities/category.entity';
 
-@Entity()
-export class Instrument {
+@Entity({
+  name: 'INSTRUMENTOS',
+})
+export class Instruments {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({
     type: 'varchar',
     length: 50,
-    nullable: false,
+    unique: true,
   })
   name: string;
 
-  @Column({ nullable: false, type: 'text' })
+  @Column({
+    nullable: false,
+    type: 'text',
+  })
   description: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    nullable: false,
+  })
+  price: number;
 
   @Column({ default: true })
   available: boolean;
+
+  @Column({
+    type: 'text',
+    default: 'No image',
+  })
+  imgUrl?: string;
+
+  @ManyToOne(() => Category, (category) => category.Instruments, {
+    eager: true,
+  })
+  category: Category;
 
   @ManyToOne(() => Studio, (studio) => studio.instruments, {
     onDelete: 'CASCADE',
